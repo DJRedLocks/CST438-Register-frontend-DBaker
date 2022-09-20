31a960880a9882173042e978fad9e72ae401a16f
DJRedLocks <david.j.baker.83@gmail.com>
2022-09-20 14:54:15 -0700

    added React components to add a student...working on fetch

--- src/components/AddStudent.js
+++ src/components/AddStudent.js
@@ -6,13 +6,22 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import { Link } from 'react-router-dom'
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Cookies from 'js-cookie';
+import {SERVER_URL} from '../constants.js'
+
 
 
 // properties addStudent is required, function called when Add clicked.
 class AddStudent extends Component {
       constructor(props) {
       super(props);
-      this.state = {open: false, course:{ } };
+      this.state = {open: false, student:{ student_name: '', student_email: '', student_statusCode:'' } };
+	  //this.state = {student: {student_name: '', student_email: '', student_statusCode:'' }};
+
     };
     
     handleClickOpen = () => {
@@ -24,25 +33,98 @@ class AddStudent extends Component {
     };
 
     handleChange = (event) => {
-      this.setState({student:{student_id: event.target.value}});
+      this.setState({student:{student_email: event.target.value}});
+	  this.setState({student:{student_name: event.target.value}});
+  	  this.setState({student:{student_statusCode: 0}});
+
     }
 
-  // Save course and close modal form
+  // Save Student and close modal form
     handleAdd = () => {
        this.props.addStudent(this.state.student);
        this.handleClose();
     }
+	
+	// Add student
+	addStudent = (student) => {
+		const token = Cookies.get('XSRF-TOKEN');
+	 
+		fetch(`${SERVER_URL}/addStudent`,
+		  { 
+			method: 'POST', 
+			headers: { 'Content-Type': 'application/json',
+					   'X-XSRF-TOKEN': token  }, 
+			body: JSON.stringify(student)
+		  })
+		.then(res => {
+			if (res.ok) {
+			  toast.success("Student successfully added", {
+				  position: toast.POSITION.BOTTOM_LEFT
+			  });
+			  //this.fetchCourses();
+			} else {
+			  toast.error("Error when adding", {
+				  position: toast.POSITION.BOTTOM_LEFT
+			  });
+			  console.error('Post http status =' + res.status);
+			}})
+		.catch(err => {
+		  toast.error("Error when adding", {
+				position: toast.POSITION.BOTTOM_LEFT
+			});
+			console.error(err);
+		})
+	} 
+	
+	
 
     render()  { 
+	/*
+		return (
+			<div>
+				<h4>Add Student</h4>
+					
+					<form>
+						<label>
+							Student name:&nbsp;
+						</label>
+						<input type="text" name="student_name" value={this.state.student_name} />
+					</form>
+					
+					<form>
+						<label>
+							Student email:&nbsp;
+						</label>
+						<input type="text" name="student_email" value={this.state.student_email} />
+					</form>
+				
+				<Button id="AddStudent" variant="outlined" color="primary" style=  {{margin: 10}} onClick={this.handleSubmit} >
+					Add Student
+				</Button>
+				
+				<Button component={Link} to={{pathname:'/'}} 
+					variant="outlined" color="primary" style={{margin: 10}}>
+					Home
+				</Button>
+				
+			</div>
+		);
+	*/
+	
+	
       return (
           <div>
             <Button variant="outlined" color="primary" style={{margin: 10}} onClick={this.handleClickOpen}>
               Add Student
             </Button>
+			<Button component={Link} to={{pathname:'/'}} 
+				variant="outlined" color="primary" style={{margin: 10}}>
+			  Home
+			</Button>
             <Dialog open={this.state.open} onClose={this.handleClose}>
                 <DialogTitle>Add Student</DialogTitle>
                 <DialogContent  style={{paddingTop: 20}} >
-                  <TextField autoFocus fullWidth label="Student Id" name="student_id" onChange={this.handleChange}  />
+                  <TextField autoFocus fullWidth label="Student Email" name="student_email" onChange={this.handleChange}  />
 				  <TextField autoFocus fullWidth label="Student Name" name="student_name" onChange={this.handleChange}  /> 
                 </DialogContent>
                 <DialogActions>
@@ -51,11 +133,13 @@ class AddStudent extends Component {
                 </DialogActions>
               </Dialog>      
           </div>
+	  
       ); 
+	
     }
 }
 
-// required property:  addCourse is a function to call to perform the Add action
+// required property:  addStudent is a function to call to perform the Add action
 AddStudent.propTypes = {
   addStudent : PropTypes.func.isRequired
 }
